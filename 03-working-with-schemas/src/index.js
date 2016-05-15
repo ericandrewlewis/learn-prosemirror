adffb679e214964a2072a6b6e3af3f07ffb7a745
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2016-05-15 16:40:55 -0400

    schema example

--- 03-working-with-schemas/src/index.js
+++ 03-working-with-schemas/src/index.js
@@ -1,9 +1,16 @@
 import {ProseMirror} from "prosemirror/dist/edit"
+import {Schema, SchemaSpec, Doc, Paragraph, Heading, Text, defaultSchema} from "prosemirror/dist/model"
+import {fromHTML} from "prosemirror/dist/format"
 import "prosemirror/dist/inputrules/autoinput"
 import "prosemirror/dist/menu/tooltipmenu"
 import "prosemirror/dist/menu/menubar"
-import {Schema, SchemaSpec, Doc, Paragraph, Heading, Text} from "prosemirror/dist/model"
-import {fromHTML} from "prosemirror/dist/format"
+
+let pm1 = window.pm1 = new ProseMirror({
+  place: document.querySelector("#editor-1"),
+  doc: fromHTML( defaultSchema, "<p>This is a document based on the default schema.</p>" ),
+  menuBar: true,
+  tooltipMenu: true
+})
 
 // :: SchemaSpec
 // The specification for the default schema.
@@ -18,8 +25,8 @@ const schemaSpec = new SchemaSpec({
 
 const schema = new Schema(schemaSpec)
 
-let pm = window.pm = new ProseMirror({
-  place: document.querySelector("#editor"),
+let pm2 = window.pm2 = new ProseMirror({
+  place: document.querySelector("#editor-2"),
   doc: fromHTML( schema, "<p>Hi</p>" ),
   schema: schema,
   menuBar: true,
