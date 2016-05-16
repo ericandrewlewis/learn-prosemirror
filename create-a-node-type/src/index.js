60ab1547d7faa3d419a42f140404b2983e51e06d
Eric Lewis <eric.andrew.lewis@gmail.com>
2016-05-16 18:19:11 -0400

    Create a node type example

--- create-a-node-type/src/index.js
+++ create-a-node-type/src/index.js
@@ -1,23 +1,25 @@
 import {ProseMirror} from "prosemirror/dist/edit"
+import {fromHTML} from "prosemirror/dist/format"
+import {Schema, defaultSchema} from "prosemirror/dist/model"
 import "prosemirror/dist/inputrules/autoinput"
 import "prosemirror/dist/menu/tooltipmenu"
 import "prosemirror/dist/menu/menubar"
-import {Schema, defaultSchema} from "prosemirror/dist/model"
 import BlockOfColor from "./block-of-color"
-
-const place = document.querySelector("#editor")
-const initialContent = document.querySelector("#initial-content")
 
 const customSchema = new Schema(defaultSchema.spec.update({block_of_color: BlockOfColor}))
 
 const pm = window.pm = new ProseMirror({
-  place: place,
-  doc: initialContent,
-  docFormat: "dom",
-  schema: customSchema
+  place: document.querySelector("#editor"),
+  doc: fromHTML( customSchema, "<p>Here's an <em>editor</em> with <b>default content</b>.</p>" ),
+  schema: customSchema,
+  menuBar: true,
+  tooltipMenu: true
 })
-
-initialContent.style.display = "none"
 
 // To insert the block...
 // pm.tr.replaceSelection(pm.schema.nodes.block_of_color.create()).apply()
+
+document.querySelector('.insert-block-button').addEventListener('click', () => {
+  let blockOfColorNode = pm.schema.nodes.block_of_color.create()
+  pm.tr.replaceSelection(blockOfColorNode, true).apply()
+})
